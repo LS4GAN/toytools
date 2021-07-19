f84b6223394153bd81f12719315bb2c58d3a079f
Dmitrii Torbunov <dtorbunov@bnl.gov>
2021-07-19 09:54:50 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import setuptools
+
+setuptools.setup(
+    name             = 'toytools',
+    version          = '0.0.1',
+    author           = 'LS4GAN Group',
+    author_email     = 'dtorbunov@bnl.gov',
+    classifiers      = [
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    description      = 'A collection of functions to handle toyzero dataset',
+    packages         = setuptools.find_packages(
+        include = [ 'toytools', 'toytools.*' ]
+    ),
+    scripts          = [ 'scripts/preprocess', ],
+    install_requires = [ 'numpy', 'pandas', 'matplotlib' ],
+)
+
