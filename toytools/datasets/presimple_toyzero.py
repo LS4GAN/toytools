f74d42d37ca498f59e8625b2b8a6b6e050779031
Dmitrii Torbunov <dtorbunov@bnl.gov>
2021-07-20 08:27:05 -0500

    datasets: Fix spelling error in dostrings

--- toytools/datasets/presimple_toyzero.py
+++ toytools/datasets/presimple_toyzero.py
@@ -25,7 +25,7 @@ class PreSimpleToyzeroDataset(GenericDataset):
         `is_train` flag controls overall behavior of the
         `SimpleToyzeroDataset`. If `is_train` is True, then the cropped regions
         of toyzero images are selected at random. Otherwise, the cropped
-        regions are selected in a repdorucible manner using `seed` parameter.
+        regions are selected in a reproducible manner using `seed` parameter.
         This flag also controls which part of the entire toyzero dataset this
         object will focus on.
         Default: False
