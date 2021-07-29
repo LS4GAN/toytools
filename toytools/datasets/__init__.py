5bdb90f5cf8a6dbf0329aa8d931dac6f534c37a9
Dmitrii Torbunov <dtorbunov@bnl.gov>
2021-07-29 10:39:59 -0400

    datasets: Strengthen pytorch check
    Previous pytorch check was insufficient and resulted in crashed when
    pytorch is absent.

--- toytools/datasets/__init__.py
+++ toytools/datasets/__init__.py
@@ -12,7 +12,8 @@ __all__ = [
 ]
 
 # NOTE: it imports torch-dependent functions conditionally on pytorch presence
-if importlib.util.find_spec("torch.utils") is not None:
-    from .torch_funcs import get_toyzero_dataset_torch
-    __all__.append('get_toyzero_dataset_torch')
+if importlib.util.find_spec("torch") is not None:
+    if importlib.util.find_spec("torch.utils") is not None:
+        from .torch_funcs import get_toyzero_dataset_torch
+        __all__.append('get_toyzero_dataset_torch')
 
