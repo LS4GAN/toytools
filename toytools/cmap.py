5c30926e1b8fcb8bca4ee5ec4773febe5dc915df
Dmitrii Torbunov <dtorbunov@bnl.gov>
2021-11-12 13:03:28 -0500

    plot: Implement support for cmap selection

---
+++ toytools/cmap.py
@@ -0,0 +1,8 @@
+"""Definitions of custom colormaps"""
+
+def get_custom_cmap_norm(cmap, vrange, log = False):
+    """Main selector for custom colormaps"""
+    return None
+
+
+
--- toytools/plot.py
+++ toytools/plot.py
@@ -6,6 +6,8 @@ import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
+from .cmap import get_custom_cmap_norm
+
 def decorate_axes(
     ax, ticks = False, grid = False, minor = False, label = False
 ):
@@ -47,10 +49,43 @@ def get_common_images_range(images, margin = 0.3, symmetric = True):
 
     return (vmin, vmax)
 
+def get_data_norm(vrange, log = False):
+    """Determine best normalization for default plot"""
+    one_sided = (
+        ((vrange[0] >= 0) and (vrange[1] >= 0))
+        or
+        ((vrange[0] <= 0) and (vrange[1] <= 0))
+    )
+
+    if log:
+        if all(v > 0 for v in vrange):
+            return mpl.colors.LogNorm(vmin = vrange[0], vmax = vrange[1])
+        else:
+            # pylint: disable=unexpected-keyword-arg
+            return mpl.colors.SymLogNorm(
+                linthresh = 1.0, vmin = vrange[0], vmax = vrange[1]
+            )
+    else:
+        if one_sided:
+            return mpl.colors.Normalize(vrange[0], vrange[1])
+        else:
+            return mpl.colors.TwoSlopeNorm(0, vrange[0], vrange[1])
+
+def get_cmap_norm(cmap, vrange, log = False):
+    """Select colormap and data normalization"""
+    result = get_custom_cmap_norm(cmap, vrange, log)
+    if result is not None:
+        return result
+
+    cmap = mpl.cm.get_cmap(cmap)
+    norm = get_data_norm(vrange, log)
+
+    return (cmap, norm)
+
 # pylint: disable=too-many-arguments
 def default_image_plot(
     ax, image, vrange = None, ticks = False, grid = False, symmetric = True,
-    symlog = False
+    log = False, cmap = 'seismic'
 ):
     """Plot toyzero `image` with default style."""
     decorate_axes(ax, ticks, grid, minor = True, label = False)
@@ -58,15 +93,7 @@ def default_image_plot(
     if vrange is None:
         vrange = get_common_images_range([ image, ], symmetric = symmetric)
 
-    # pylint: disable=no-member
-    cmap = mpl.cm.seismic
-
-    if symlog:
-        norm = mpl.colors.SymLogNorm(
-            linthresh = 1.0, vmin = vrange[0], vmax = vrange[1]
-        )
-    else:
-        norm = mpl.colors.TwoSlopeNorm(0, vrange[0], vrange[1])
+    cmap, norm = get_cmap_norm(cmap, vrange, log)
 
     return ax.imshow(image, cmap = cmap, norm = norm)
 
