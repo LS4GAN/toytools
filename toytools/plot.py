6b2da6ab7a908bc2582708fd68fe6906facec333
Dmitrii Torbunov <dtorbunov@bnl.gov>
2021-11-12 11:40:41 -0500

    Improve color range calculation
    Previously, it was implicitly assumed that the data is symmetric with
    respect to zero. This commit adds explicit support for the case when
    data is not symmetric.

--- toytools/plot.py
+++ toytools/plot.py
@@ -32,40 +32,41 @@ def decorate_axes(
                 True, which = 'minor', linestyle = 'dashed', linewidth = 0.5
             )
 
-def get_common_images_range(images, margin = 0.3):
+def get_common_images_range(images, margin = 0.3, symmetric = True):
     """Get common color range across multiple images"""
 
     vmax = max([ np.max(image) for image in images ])
-    vmax = max(vmax, 1)
     vmax = (1 + margin) * vmax
 
     vmin = min([ np.min(image) for image in images ])
-    vmin = min(vmin, -1)
     vmin = (1 + margin) * vmin
+
+    if symmetric:
+        vmax = max(vmax, -vmin)
+        vmin = -vmax
 
     return (vmin, vmax)
 
 # pylint: disable=too-many-arguments
 def default_image_plot(
-    ax, image, vmin = None, vmax = None, ticks = False, grid = False,
+    ax, image, vrange = None, ticks = False, grid = False, symmetric = True,
     symlog = False
 ):
     """Plot toyzero `image` with default style."""
     decorate_axes(ax, ticks, grid, minor = True, label = False)
 
-    if vmin is None:
-        vmin = min(np.min(image), -1)
-
-    if vmax is None:
-        vmax = max(np.max(image), 1)
+    if vrange is None:
+        vrange = get_common_images_range([ image, ], symmetric = symmetric)
 
     # pylint: disable=no-member
     cmap = mpl.cm.seismic
 
     if symlog:
-        norm = mpl.colors.SymLogNorm(linthresh = 1.0, vmin = vmin, vmax = vmax)
+        norm = mpl.colors.SymLogNorm(
+            linthresh = 1.0, vmin = vrange[0], vmax = vrange[1]
+        )
     else:
-        norm = mpl.colors.TwoSlopeNorm(0, vmin, vmax)
+        norm = mpl.colors.TwoSlopeNorm(0, vrange[0], vrange[1])
 
     return ax.imshow(image, cmap = cmap, norm = norm)
 
