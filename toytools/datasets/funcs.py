d687499f078c8a6dfaf5c6bc34da03bcd0d46317
Dmitrii Torbunov <dtorbunov@bnl.gov>
2022-03-14 18:39:48 -0400

    datasets: Add selectors for v1 toyzero datasets

--- toytools/datasets/funcs.py
+++ toytools/datasets/funcs.py
@@ -1,9 +1,11 @@
 """Various helper functions to construct toyzero datasets."""
 
-from .simple_toyzero       import SimpleToyzeroDataset
-from .presimple_toyzero    import PreSimpleToyzeroDataset
-from .preunaligned_toyzero import PreUnalignedToyzeroDataset
-from .precropped_toyzero   import PreCroppedToyzeroDataset
+from .simple_toyzero          import SimpleToyzeroDataset
+from .presimple_toyzero       import PreSimpleToyzeroDataset
+from .presimple_toyzero_v1    import PreSimpleToyzeroDatasetV1
+from .preunaligned_toyzero    import PreUnalignedToyzeroDataset
+from .precropped_toyzero      import PreCroppedToyzeroDataset
+from .precropped_toyzero_v1   import PreCroppedToyzeroDatasetV1
 
 def get_toyzero_dataset(name, path, **data_args):
     """Return toyzero dataset based on its name"""
@@ -19,6 +21,12 @@ def get_toyzero_dataset(name, path, **data_args):
 
     if name == 'toyzero-precropped':
         return PreCroppedToyzeroDataset(path, **data_args)
+
+    if name == 'toyzero-precropped-v1':
+        return PreCroppedToyzeroDatasetV1(path, **data_args)
+
+    if name == 'toyzero-presimple-v1':
+        return PreSimpleToyzeroDatasetV1(path, **data_args)
 
     raise ValueError("Unknown toyzero dataset name: %s" % name)
 
